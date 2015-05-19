d0231155e9b34f8784886aa7c0120682b789ab37
Barry Wark <barry@physion.us>
2015-05-19 13:59:33 -0400

    Update once per day

--- BarcodePrinter/Properties/AssemblyInfo.cs
+++ BarcodePrinter/Properties/AssemblyInfo.cs
@@ -51,8 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.1")]
-[assembly: AssemblyFileVersion("1.1.0.1")]
-
-// Mark this assembly as Squirrel aware
-//[assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
+[assembly: AssemblyVersion("1.1.0.10")]
+[assembly: AssemblyFileVersion("1.1.0.10")]
